3d38d8b47784f2d6ac4cdb642060bdf938b08998
ShinHyungJune <ssa4141@naver.com>
2023-12-26 01:48:30 +0900

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-window.domain = "http://localhost:90";
-// window.domain = "http://hi-secure.ufeed.co.kr";
+// window.domain = "http://localhost:90";
+window.domain = "http://hi-secure.ufeed.co.kr";
 function openLoading(){
     $("html").append('<span class="loader"></span>')
 }
