119b0d8a7d1813e90f0d59b129b68a59b0912f81
ShinHyungJune <ShinHyungJune>
2024-01-10 22:42:03 +0900

    생성일자 인덱싱 추가

--- database/migrations/2024_01_10_224046_change_created_at_column_to_histories_table.php
+++ database/migrations/2024_01_10_224046_change_created_at_column_to_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeHistoriesColumnToHistoriesTable extends Migration
+class ChangeCreatedAtColumnToHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class ChangeHistoriesColumnToHistoriesTable extends Migration
     public function up()
     {
         Schema::table('histories', function (Blueprint $table) {
-            $table->dateTime("logged_at")->nullable()->index()->change();
-            $table->index(["device_id", "logged_at"]);
+            $table->timestamp("created_at")->index()->change();
         });
     }
 
