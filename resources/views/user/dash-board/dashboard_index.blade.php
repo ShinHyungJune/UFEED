d53136cd300827557cdd7f07076f33835192dc36
ShinHyungJune <ssa4141@naver.com>
2024-02-01 23:56:43 +0900

    fix

--- resources/views/user/dash-board/dashboard_index.blade.php
+++ resources/views/user/dash-board/dashboard_index.blade.php
@@ -2130,7 +2130,7 @@
 <!--                                </p>-->
                             </div>
                             <p class="byte">
-                                ${((parseInt(rankingTraffic.byte) * 8) / 1000000).toFixed(1)} Mbps
+                                ${((parseInt(rankingTraffic.byte) * 8) / 1000000).toLocaleString()} Mbps
                             </p>
                         </div>
                     </div>`;
