1f863e9a6a487eeeb15e225b48a61c799a5e02ed
clover <ThreeLeafClover9@gmail.com>
2024-03-12 19:27:44 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,13 @@ Route::get("/test", function(){
             "apikey" => "f4ef6356d46d6c08bf7687f3f705482c"
         ],
     ]);
-//    $start = Carbon::now()->subHours(24)->format('Y-m-d\TH:i:s');
-    $start = Carbon::parse('2024-03-01T04:46:20')->format('Y-m-d\TH:i:s');
+    $start = Carbon::now()->subHours(24)->format('Y-m-d\TH:i:s');
     $end = Carbon::now()->format('Y-m-d\TH:i:s');
     $domain = 'https://10.0.1.251:58005';
 
     $response = $client->request("get", "{$domain}/restapi/tm/v1/log/search?searchType=CUSTOM&startDate={$start}&endDate={$end}&pageSize=1&pageNo=1&query=module:tgIps and group:DDoS");
 
-    dd($response->getBody());
+    dd($start, $end, json_decode($response->getBody()));
 
 //    $history = new \App\Models\History();
 //    \App\Models\StatusHistory::record();
