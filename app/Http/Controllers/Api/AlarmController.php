2919352a64cadcb6fbad8668733c364787f4758a
clover <ThreeLeafClover9@gmail.com>
2024-05-31 10:46:53 +0900

    test

--- app/Http/Controllers/Api/AlarmController.php
+++ app/Http/Controllers/Api/AlarmController.php
@@ -31,9 +31,6 @@ class AlarmController extends ApiController
         UserLog::create([
             'user_id' => 1, 'user_ids' => '1', 'ip_address' => 'alarm test', 'activity' => json_encode($request->json())
         ]);
-        UserLog::create([
-            'user_id' => 1, 'user_ids' => '1', 'ip_address' => 'alarm test', 'activity' => json_encode($request->data)
-        ]);
         $data = json_decode($request->data);
 
         $device = Device::where("title", $data->device)->first();
