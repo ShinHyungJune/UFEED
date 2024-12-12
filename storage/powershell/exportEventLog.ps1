4245a63b1454daa38a04660fbb06f88f7c835016
clover <ThreeLeafClover9@gmail.com>
2024-12-12 09:03:29 +0900

    event log

--- storage/powershell/exportEventLog.ps1
+++ storage/powershell/exportEventLog.ps1
@@ -1,4 +1,4 @@
-$eventIDs = @(41, 1074, 6006, 6008, 4624, 4625, 4634);
+$eventIDs = @(7, 8, 9, 41, 104, 200, 201, 204, 1074, 6006, 6008, 4624, 4625, 4634, 8202);
 $timeLimit = (Get-Date).AddMinutes(-1);
 $csvPath = "C:\Temp\FilteredSystemEvents.csv"
 Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID -and $_.TimeGenerated -gt $timeLimit } | Select-Object TimeGenerated, EntryType, Source, EventID, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
