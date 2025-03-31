a8d7f1d64126c47994d65b880e08a7cb2bdea5e4
clover <ThreeLeafClover9@gmail.com>
2025-03-31 13:55:37 +0900

    terminal

--- storage/powershell/exportEventLog.ps1
+++ storage/powershell/exportEventLog.ps1
@@ -2,3 +2,6 @@ $eventIDs = @(7, 8, 9, 21, 23, 24, 25, 41, 104, 200, 201, 204, 1074, 6006, 6008,
 $timeLimit = (Get-Date).AddMinutes(-1);
 $csvPath = "C:\Temp\FilteredSystemEvents.csv"
 Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID -and $_.TimeGenerated -gt $timeLimit } | Select-Object TimeGenerated, EntryType, Source, EventID, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+
+#초기세팅
+#Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID } | Select-Object TimeGenerated, EntryType, Source, EventID, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
--- storage/powershell/exportRemoteLog.ps1
+++ storage/powershell/exportRemoteLog.ps1
@@ -1,3 +1,6 @@
 $timeLimit = (Get-Date).AddMinutes(-1);
 $csvPath = "C:\Temp\RDPLogs.csv"
-Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" | Where-Object { $_.TimeGenerated -gt $timeLimit } | Select-Object TimeCreated, Id, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational" | Where-Object { $_.Id -eq 1149 -and $_.TimeGenerated -gt $timeLimit } | Select-Object TimeCreated, Id, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+
+#초기세팅
+#Get-WinEvent -LogName "Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational" | Where-Object { $_.Id -eq 1149 } | Select-Object TimeCreated, Id, Message | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
